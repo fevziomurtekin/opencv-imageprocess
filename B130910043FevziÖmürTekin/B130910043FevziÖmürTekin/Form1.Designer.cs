4a5785cdbe22729ae7b3fc94e812921ec9470b85
Fevzi Ömür Tekin <fevzi.tekin@ogr.sakarya.edu.tr>
2016-05-11 19:49:50 +0300

    C# face recognition example

---
+++ B130910043FevziÖmürTekin/B130910043FevziÖmürTekin/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace CSharp_Proje_Ödevi_Deneme_vol11._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(22, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Resimi Seçiniz";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(368, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(380, 414);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(210, 238);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FloralWhite;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(22, 238);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 55);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Resimi Boyutu Girin";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(210, 273);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(178, 241);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(178, 276);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Y";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(779, 448);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Name = "Form1";
+            this.Text = "Yüz Algılama Programı";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ B130910043FevziÖmürTekin/B130910043FevziÖmürTekin/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using OpenCvSharp;
+using OpenCvSharp.MachineLearning;
+using OpenCvSharp.Blob;
+using OpenCvSharp.UserInterface;
+using OpenCvSharp.CPlusPlus;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp.Extensions;
+using System.IO;
+
+
+
+
+namespace CSharp_Proje_Ödevi_Deneme_vol11._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool durum = false;
+
+            OpenFileDialog ds = new OpenFileDialog();
+            ds.Title = "Resim seç";
+            ds.Filter = "(*.jpg) | *.jpg ";
+
+            if (ds.ShowDialog() == DialogResult.OK)
+            {
+                this.pictureBox1.Image = new Bitmap(ds.OpenFile());
+                durum = true;
+            }
+
+            var haarCascade = new CascadeClassifier("C:\\Users\\Ömür\\Desktop\\haarcascade_frontalface_alt2.xml");
+            var gray = new Mat(ds.FileName, LoadMode.GrayScale);
+            Rect[] faces = haarCascade.DetectMultiScale(gray, 1.08, 2, HaarDetectionType.ScaleImage, new OpenCvSharp.CPlusPlus.Size(30, 30));
+            int yüzSayısı = faces.Length;
+
+
+            if (durum)
+            {
+                if (!Directory.Exists(@"C:\Users\Ömür\Desktop\Resimler"))
+                    Directory.CreateDirectory(@"C:\Users\Ömür\Desktop\Resimler");
+                if (!Directory.Exists(@"C:\Users\Ömür\Desktop\Resimler\insan"))
+                    Directory.CreateDirectory(@"C:\Users\Ömür\Desktop\Resimler\insan");
+                if (!Directory.Exists(@"C:\Users\Ömür\Desktop\Resimler\Manzara"))
+                    Directory.CreateDirectory(@"C:\Users\Ömür\Desktop\Resimler\Manzara");
+                string imagePath = ds.FileName.ToString();
+                string imagepath = imagePath.ToString();
+                if (ds.FileName.ToString() != "")
+                {
+
+                    imagepath = imagepath.Substring(imagepath.LastIndexOf("\\"));
+                    imagepath = imagepath.Remove(0, 1);
+                }
+
+                if (yüzSayısı < 1)
+                {
+                    File.Copy(ds.FileName, @"C:\Users\Ömür\Desktop\Resimler\Manzara\" + imagepath.ToString());
+                    //durum = false;
+
+                }
+
+                else
+                {
+                    File.Copy(ds.FileName, @"C:\Users\Ömür\Desktop\Resimler\insan\" + imagepath.ToString());
+
+
+
+
+                }
+
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int xKoordinatı, yKoordinatı;
+            xKoordinatı = Int32.Parse(textBox1.Text);
+            yKoordinatı = Int32.Parse(textBox2.Text);
+            pictureBox1.Size = new System.Drawing.Size(xKoordinatı, yKoordinatı);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+    }
+}
